b2ac0598b64cd9f43425d3430f2d037228eb613a
duonqHoang <duonghoang.v06@gmail.com>
2022-10-10 13:07:34 +0700

    Init

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+/*
+* Calculate BMI
+* @param weight in kg
+* @param height in m
+*/
+float bmi(float weight, float height) {
+    // weight and height are positive
+    // weight smaller than heaviest person
+    // height smaller than tallest person
+    if (weight <=0 || height <= 0 || weight > 700 || height > 3) {
+        return -1;
+    }
+    return weight / (height * height);
+}
+
+//Category of BMI
+string bmi_category(float bmi) {
+    if (bmi < 0) return "Invalid"; 
+    if (bmi < 18.5) {
+        return "Underweight";
+    } else if (bmi < 25.0) {
+        return "Normal";
+    } else if (bmi < 30.0) {
+        return "Overweight";
+    } else {
+        return "Obese";
+    }
+}
+
+int main(){
+    //Test cases
+    //Kiem thu gia tri bien
+    assert(bmi_category(bmi(0, 1.5)) == "Invalid");
+    assert(bmi_category(bmi(0.1, 1.5)) == "Underweight");
+    assert(bmi_category(bmi(300, 1.5)) == "Obese");
+    assert(bmi_category(bmi(699.5, 1.5)) == "Obese");
+    assert(bmi_category(bmi(700, 1.5)) == "Obese");
+    assert(bmi_category(bmi(700.1, 1.5)) == "Invalid");
+
+    assert(bmi_category(bmi(300, 0)) == "Invalid");
+    assert(bmi_category(bmi(300, 0.1)) == "Obese");
+    assert(bmi_category(bmi(300, 2.9)) == "Obese");
+    assert(bmi_category(bmi(300, 3)) == "Obese");
+    assert(bmi_category(bmi(300, 3.1)) == "Invalid");
+
+    //Kiem thu phan hoach tuong duong
+    assert(bmi_category(bmi(300, 1.5)) == "Obese");
+    assert(bmi_category(5) == "Underweight");
+    assert(bmi_category(20) == "Normal");
+    assert(bmi_category(27) == "Overweight");
+    assert(bmi_category(50) == "Obese");
+
+    //Program
+    float weight, height;
+    cout << "Enter weight (kg): ";
+    cin >> weight;
+    cout << "Enter height (m): ";
+    cin >> height;
+
+    float bmi_value = bmi(weight, height);
+    string category = bmi_category(bmi_value);
+    
+    cout << "BMI: " << bmi_value << endl;
+    cout<< "Category: " << category << endl;
+
+    return 0;
+}
